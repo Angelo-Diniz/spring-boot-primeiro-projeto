870f7a5228aa09717a6200ea83af37d140fc5375
Angelo Diniz <angelo.jose.7l@hotmail.com>
2020-06-29 20:39:09 -0300

    Git init project

---
+++ src/main/java/com/diniz/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,15 @@
+package com.diniz.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+		System.out.println("Meu primeiro projeto...");
+		
+	}
+
+}
